825afacc5b22c4ae5ef8aad4683d5cec0a1c703f
Alun Turner <alun.turner@googlemail.com>
2020-08-29 11:21:06 +0100

    complete module 8

--- server/models/teamMember.js
+++ server/models/teamMember.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
+const sizeValidator = [
+  function (val) {
+    return val.trim().length > 0 && val.trim().length <= 50;
+  },
+  "{PATH} must be between 1 and 50 characters long",
+];
+
 const teamMemberSchema = new mongoose.Schema({
-  name: { type: String },
+  name: { type: String, required: true, validate: sizeValidator },
 });
 
 module.exports = mongoose.model("TeamMember", teamMemberSchema);
